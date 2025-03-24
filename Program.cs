7d869ea23c0e2c60029bfff216754aa739e06437
Yasmim de Castro <Yas.castr0.r@gmail.com>
2025-03-24 10:08:35 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite seu primeiro Nome:");
+string nome = Console.ReadLine()!;
+Console.WriteLine("Digite seu sobrenome:");
+string sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de Catálogo: {sobrenome.ToUpper()}, {nome}");
+
+
